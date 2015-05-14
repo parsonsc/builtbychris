9d0cbefa52d63bf68dca640334729bbdb97e1fd0
parsonsc <chris.parsons@goodagency.co.uk>
2015-05-14 17:11:12 +0100

    off canvas nav start

--- js/main.js
+++ js/main.js
@@ -1,106 +1,69 @@
-// BACK TO TOP 
-$("#backTop").click(function () {
-   $("html, body").animate({scrollTop: 0}, 1200);
+/**
+ * sidebarEffects.js v1.0.0
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2013, Codrops
+ * http://www.codrops.com
+ */
+ var SidebarMenuEffects = (function() {
+
+ 	function hasParentClass( e, classname ) {
+		if(e === document) return false;
+		if( classie.has( e, classname ) ) {
+			return true;
+		}
+		return e.parentNode && hasParentClass( e.parentNode, classname );
+	}
+
+	// http://coveroverflow.com/a/11381730/989439
+	function mobilecheck() {
+		var check = false;
+		(function(a){if(/(android|ipad|playbook|silk|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
+		return check;
+	}
+
+	function init() {
+
+		var container = document.getElementById( 'site_container' ),
+			buttons = Array.prototype.slice.call( document.querySelectorAll( '#st-trigger-effects > button' ) ),
+			// event type (if mobile use touch events)
+			eventtype = mobilecheck() ? 'touchstart' : 'click',
+			resetMenu = function() {
+				classie.remove( container, 'st-menu-open' );
+			},
+			bodyClickFn = function(evt) {
+				if( !hasParentClass( evt.target, 'st-menu' ) ) {
+					resetMenu();
+					document.removeEventListener( eventtype, bodyClickFn );
+				}
+			};
+
+		buttons.forEach( function( el, i ) {
+			var effect = el.getAttribute( 'data-effect' );
+
+			el.addEventListener( eventtype, function( ev ) {
+				ev.stopPropagation();
+				ev.preventDefault();
+				container.className = 'site_container'; // clear
+				classie.add( container, effect );
+				setTimeout( function() {
+					classie.add( container, 'st-menu-open' );
+				}, 25 );
+				document.addEventListener( eventtype, bodyClickFn );
+			});
+		} );
+
+	}
+
+	init();
+
+})();
+
+$(document).ready(function(){
+	$(".close").click(function(){
+		$(".st-menu-open").removeClass("st-menu-open");
+	});
 });
-$(document).scroll(function () {
-    var y = $(this).scrollTop();
-    if (y > 500) {
-        $('#backTop').fadeIn();
-    } else {
-        $('#backTop').fadeOut();
-    }
-
-});
-// END BACK TO TOP
-
-// SMOOTH SCROLL ANCHOR
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top 
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
-// END SMOOTH SCROLL ANCHOR
-
-// MOBILE MENU
-$( document ).ready(function() {
-  var hamburger = $('#menu-icon');
-  hamburger.click(function() {
-     hamburger.toggleClass('active');
-     return false;
-  });
-});
-$('#menu-icon').click(function(){
-	$('.site_nav ul').slideToggle();
-});
-// END MOBILE MENU
-
-// STICKY NAV
-$(window).scroll(function() {    
-    var scroll = $(window).scrollTop();
-
-    if (scroll >= 375) {
-        $(".site_nav").addClass("sticky");
-    } else {
-        $(".site_nav").removeClass("sticky");
-    }
-});
-// END STICKY NAV
-
-
-$(document).ready(function() {
-
-  /***************** Waypoints ******************/
-
-  $('.work_section').waypoint(function() {
-    $('.work_section').addClass('animated fadeIn');
-  }, {
-    offset: '75%'
-  });
-  $('.fa-chevron-down').waypoint(function() {
-    $('.fa-chevron-down').addClass('animated bounceInDown');
-  }, {
-    offset: '75%'
-  });
-  $('.top').waypoint(function() {
-    $('.top').addClass('animated slideInLeft');
-  }, {
-    offset: '75%'
-  });
-  $('.wp4').waypoint(function() {
-    $('.wp4').addClass('animated fadeInDown');
-  }, {
-    offset: '75%'
-  });
-});
-
-// NAV SELECTED LI change ON SCROLL
-var sections = $('section')
-  , nav = $('nav')
-  , nav_height = nav.outerHeight();
- 
-  $(window).on('scroll', function () {
-    var cur_pos = $(this).scrollTop();
-   
-    sections.each(function() {
-      var top = $(this).offset().top -300,
-          bottom = top + $(this).outerHeight();
-   
-      if (cur_pos >= top && cur_pos <= bottom) {
-        nav.find('a').removeClass('active');
-        sections.removeClass('active');
-   
-        $(this).addClass('active');
-        nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
-      }
-    });
-});
-
